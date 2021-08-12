5896d19229ec9fdf363a26233c86b746058ffc20
kazuki <ozakazu8705@gamil.com>
2021-08-12 11:50:23 +0900

    header変更準備

--- js/main.js
+++ js/main.js
@@ -256,10 +256,10 @@ const mySwiper = new Swiper ('.swiper-container', {
     loop: true, 
     effect: 'fade',
     speed: 800,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
  
     
 })
